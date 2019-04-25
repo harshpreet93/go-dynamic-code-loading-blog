5baf7ff5f7cf093be4d25d2014918649b893d759
Harshpreet Singh <singhharshpreet1993@gmail.com>
2019-04-24 22:04:55 -0700

    start workspace readiness code

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 )
+
+const plugin_source_folder_path  = "plugin_source"
 
 func main()  {
 
@@ -14,6 +19,23 @@ func main()  {
 
 	fmt.Println("pluginrepo is "+*pluginrepo)
 
+	cloneRepo(*pluginrepo)
+
 }
 
+func cloneRepo(repo string) {
+	//make sure
+	ensure_plugin_repo_folder_readiness()
+}
 
+func ensure_plugin_repo_folder_readiness()  {
+	// clean up /var/plugin_loader/plugin_source/
+	plugin_source_path := filepath.Join(".", plugin_source_folder_path)
+	os.RemoveAll(plugin_source_path)
+	err := os.MkdirAll(plugin_source_path, os.ModePerm)
+	if err != nil {
+		log.Println("error creating plugin source folder", err)
+		return
+	}
+	
+}
